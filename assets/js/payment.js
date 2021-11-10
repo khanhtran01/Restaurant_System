6488e7159563ff30168f57c5476aac28ee2ad951
bao-nguyen-khac <69065936+bao-nguyen-khac@users.noreply.github.com>
2021-11-10 15:07:21 +0700

    xong payment view

--- assets/js/payment.js
+++ assets/js/payment.js
@@ -6,19 +6,153 @@ document.getElementById("button_payment").addEventListener("click", function () 
         var id = ele.getElementsByClassName('container_cart_item')[0].getAttribute('id');
         var quantity = ele.getElementsByClassName('cart_info_quantity')[0].textContent;
         obj.id = id;
+        obj.name = ele.querySelector('.cart_info_name').textContent;
+        obj.price = ele.querySelector('.container_cart_info_price').textContent
         obj.quantity = quantity;
+        obj.img = ele.querySelector('img').getAttribute('src')
         arrData.push(obj);
     })
     document.querySelector('.app').classList.add('displayNode')
-    var paymentView = `<div class="header">
-    <div class="header-block row">
-        <div class="back-btn col-1">
-            <em class="ti-back-left"></em>
+    var totalPrice = 0
+    var paymentView = `
+    <div class="header">
+        <div class="header-block row">
+            <div class="back-btn col-1">
+                <em class="ti-back-left"></em>
+            </div>
+            <div class="header-term col-10">THANH TOÁN</div>
         </div>
-        <div class="header-term col-10">THANH TOÁN</div>
     </div>
-</div>`
-    document.querySelector('.__payment-view').innerHTML = paymentView
-    // console.log(arrData)
+    <div class="full-content">
+        <div class="container">
+            <div class="customer-info">
+                <div class="info-header">
+                    ĐỊA CHỈ NHẬN HÀNG
+                </div>
+                <div class="info-body row">
+                    <div class="info-name_mobile col-4">
+                        <div class="info-name">Lê Hồ Hoài Bão</div>
+                        <div class="info-mobile">(+84)854557858</div>
+                    </div>
+                    <div class="info-address col-6">
+                        Phường Linh Trung, TP.Thủ Đức, TPHCM
+                    </div>
+                    <div class="info-change col-2">
+                        <div class="change_info-btn">THAY ĐỔI</div>
+                    </div>
+                </div>
+            </div>
     
+            <div class="product-list">
+                <div class="list-header row">
+                    <div class="product-name col-6">Tên món ăn</div>
+                    <div class="product-price col-2">Đơn giá</div>
+                    <div class="product-count col-2">Số lượng</div>
+                    <div class="product-total col-2">Thành tiền</div>
+                </div>
+                <div class="show-product">`
+    arrData.forEach(ele => {
+        totalPrice += +ele.price
+        paymentView += `
+        <div class="product row">
+            <div class="product-img col-1">
+                <img class="__box-img" src="${ele.img}">
+            </div>
+            <div class="product-name col-5">${ele.name}</div>
+            <div class="product-price col-2">50000đ</div>
+            <div class="product-count col-2">${ele.quantity}</div>
+            <div class="product-total col-2">${ele.price}</div>
+        </div>`
+    })
+    paymentView +=`
+                </div>
+                <div class="total row">
+                    <div class="total-blank1 col-7"></div>
+                    <div class="total-blank2 col-1"></div>
+                    <div class="total-text col-2">TỔNG TIỀN</div>
+                    <div class="total-num col-2">${totalPrice}</div>
+                </div>
+            </div>
+    
+            <div class="chooseMethod">
+                <div class="chooseMethod--title">
+                    Chọn phương thức thanh toán:
+                </div>
+                <div class="chooseMethod--choose" id="chooseMethod--choose">
+                    <ul >
+                        <li onclick="changemethod('e-banking')" class="chooseMethod--method">E-Banking</li>
+                        <li onclick="changemethod('e-wallet')" class="chooseMethod--method active--payment">E-Wallet</li>
+                    </ul>
+                </div>
+            
+                <div class="form" id="form">
+                    <form id="e-banking" class="form__e-banking hide" action="">
+                        <label for="bank">Chọn ngân hàng:</label>
+                        <select id="bank" name="bank">
+                            <option value="ACB">Ngân hàng Á Châu (ACB)</option>
+                            <option value="Agribank">Agribank</option>
+                            <option value="SHB">SHB</option>
+                        </select>
+                        <label for="seri">Số seri:</label>
+                        <input type="text" name="seri" id="seri">
+                        <p class="price">Tổng tiền : ${totalPrice}</p>
+                        <input type="submit" value="Thanh toán">
+        
+                    </form>
+        
+                    <form id="e-wallet" class="form__e-wallet" action="">
+                        <label for="bank">Chọn Ví điện tử:</label>
+                        <select id="e-wallet" name="e-wallet">
+                            <option value="zalo">Zalo Pay</option>
+                            <option value="momo">Momo</option>
+                        </select>
+                        <label for="seri">Số điện thoại:</label>
+                        <input type="text" name="phone" id="phone" placeholder="Phone number">
+                        <p class="price">Tổng tiền : 195000đ</p>
+        
+                        <input type="submit" value="Thanh toán">
+                    </form>
+                </div>
+                
+            </div>
+        </div>
+    </div>`
+    // Add active class to the current button (highlight it)
+    
+
+    document.querySelector('.__payment-view').innerHTML = paymentView
+    document.querySelector('.back-btn').addEventListener("click", function () {
+        document.querySelector('.app').classList.remove('displayNode')
+        document.querySelector('.__payment-view').innerHTML = ''
+    })
+    // console.log(arrData)
+
 });
+var header = document.getElementById("chooseMethod--choose");
+    var btns = header.getElementsByClassName("chooseMethod--method");
+
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function () {
+            var current = document.getElementsByClassName("active--payment");
+            if (current.length > 0) {
+                current[0].className = current[0].className.replace(" active--payment", "");
+            }
+            this.className += " active--payment";
+        });
+    }
+
+
+    function changemethod(id) {
+        if (id == "e-wallet") {
+            current = document.getElementById(id);
+            current.classList.remove("hide")
+            document.getElementById("e-banking").className += " hide";
+        }
+        if (id == "e-banking") {
+            current = document.getElementById(id);
+            current.classList.remove("hide")
+            document.getElementById("e-wallet").className += " hide";
+        }
+    }
+
+
--- mvc/views/home.php
+++ mvc/views/home.php
@@ -16,11 +16,14 @@ $home_url = getUrl().'/';
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css"
         integrity="sha512-oHDEc8Xed4hiW6CxD7qjbnI+B07vDdX7hEPTvn9pSZO1bcRqHp8mj9pyr+8RVC2GmtEfI2Bi9Ke9Ass0as+zpg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./assets/css/payment.css">
+    <link rel="stylesheet" href="./assets/fontawesome-free-5.15.4-web/css/all.css">
+    <link rel="stylesheet" href="./assets/themify-icons/themify-icons.css">
     <title>Document</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
    
 </head>
 <body>
@@ -29,8 +32,8 @@ $home_url = getUrl().'/';
         <?php require_once "./mvc/views/pages/homePages.php";?>
         
     </div>
-    <div class="__payment-view">
-
+    <div class="__payment-view ">
+    
     </div>
     
 
