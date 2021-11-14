e804161ceb1f327e8f3747feaea98d58fec7be30
pvdat2k1 <dat01232011743@gmail.com>
2021-11-14 21:44:30 +0700

    Cập nhật footer mới
    Co-Authored-By: Nguyễn Minh Bảo <85435173+minhbao20052001@users.noreply.github.com>

--- mvc/views/blocks/footer.php
+++ mvc/views/blocks/footer.php
@@ -10,7 +10,7 @@
 
     </div>
 </div> -->
-<style>
+<!-- <style>
 .footer_page{
     background-color: #f5f5f5;
     width: 100%;
@@ -65,4 +65,246 @@
         </div>
 
     </div>
-</div>
+</div> -->
+<!-- <div class="footer_page">
+    <div class="info">
+        <div class="info-title"><strong>BK FOOD</strong></div>
+        <div class="info-location"><strong>Địa chỉ:</strong> 42/24 - 42/26 Đường 643 Tạ Quang Bửu, phường 4, quận 8, Hồ Chí Minh</div>
+        <div class="info-phone"><strong>Điện thoại: </strong> 0392747972 - 0392747972</div>
+        <div class="info-email"><strong>Email: </strong>MinhBao@gmail.com</div>
+    </div>
+    <div class="member">
+
+    </div>
+</div> -->
+
+<!-- NEW FOOTER -->
+
+<style>
+    .footer_page{
+        background-color: #f5f5f5;
+        width: 100%;
+    }
+    .info{
+        margin: 10px;
+        font-size: 12px;
+    }
+    .footer_content{
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+    }
+    .member{
+        display:flex;
+    }
+    .member_1{
+        margin: 10px;
+    }
+    .member_2{
+        margin: 10px;
+        margin-top: 25px;
+    }
+    .item_list{
+        margin-bottom: 15px;
+    }
+
+/*  */
+
+    * {
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    footer {
+        position: relative;
+        width: 100%;
+        height: auto;
+        padding: 50px 100px;
+        background: rgb(180, 180, 180);;
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+    }
+
+    footer .container {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        flex-direction: row;
+    }
+
+    footer .container .noi-dung{
+        margin-right: 30px;
+    }
+
+    footer  .container .noi-dung.about{
+        width:40%;
+    }
+
+    footer .container .noi-dung.about h2{
+        position: relative;
+        color: black;
+        font-weight: 500;
+        margin-bottom: 15px;
+    }
+    footer .container .noi-dung.about h2:before{
+        content: '';
+        position: absolute;
+        bottom: -5px;
+        left: 0;
+        width: 50px;
+        height: 2px;
+        background: #f00;
+    }
+    footer .container .noi-dung.about p{
+        color: black;
+    }
+    /*Thiết Lập Cho Thành Phần Icon Mạng Xã Hội*/
+    .social-icon{
+        margin-top: 20px;
+        padding-left: 0;
+        display: flex;
+    }
+    .social-icon li {
+        list-style: none;
+    }
+    .social-icon li a{
+        display: inline-block;
+        width: 40px;
+        height: 40px;
+        background: #222;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-right: 10px;
+        text-decoration: none;
+        border-radius: 4px;
+    }
+
+    .social-icon li a:hover{
+        background: #f00;
+    }
+
+    .social-icon li a .fa{
+        color: white;
+        font-size: 20px;
+    }
+
+    .links h2{
+        position: relative;
+        color: black;
+        font-weight: 500;
+        margin-bottom: 15px;
+    }
+
+    .links h2{
+        position: relative;
+        color: black;
+        font-weight: 500;
+        margin-bottom: 15px;
+    }
+    .links h2::before{
+        content: '';
+        position: absolute;
+        bottom: -5px;
+        left: 0;
+        width: 50px;
+        height: 2px;
+        background: #f00;
+    }
+    .links{
+    position: relative;
+    width: 25%;
+    }
+
+    .links ul li{
+        list-style: none;
+    }
+
+    .links ul li a{
+        color: black;
+        text-decoration: none;
+        margin-bottom: 10px;
+        display: inline-block;
+    }
+    .links ul li a:hover{
+        color: black;
+    }
+
+    .contact h2{
+        position: relative;
+        color: black;
+        font-weight: 500;
+        margin-bottom: 15px;
+    }
+    .contact h2::before{
+        content: '';
+        position: absolute;
+        bottom: -5px;
+        left: 0;
+        width: 50px;
+        height: 2px;
+        background: #f00;
+    }
+    .contact{
+        width: calc(35% - 60px);
+        margin-right: 0 !important;
+    }
+    .contact .info{
+        position: relative;
+    }
+
+    .contact .info li{
+        display: flex;
+        margin-bottom: 16px;
+    }
+    .contact .info li span:nth-child(1) {
+        color: black;
+        font-size: 20px;
+        margin-right: 10px;
+    }
+    .contact .info li span{
+        color: black;
+    }
+    .contact .info li a{
+        color: black;
+        text-decoration: none;
+    }
+</style>
+<footer style="padding-left: 50px; padding-right: 50px;">
+    <div class="container">
+        <div class="noi-dung about">
+            <h2>Về Chúng Tôi</h2>
+            <p style="margin-bottom: 5px;">Cửa hàng BKFOOD</p>
+            <p>Cửa hàng thức ăn nhanh chuyên phục vụ sinh viên trường Đại học Bách Khoa, đặc biệt là sinh viên ngành Máy tính.</p>
+            <ul class="social-icon">
+                <li><a href=""><i class="fa fa-facebook"></i></a></li>
+                <li><a href=""><i class="fa fa-twitter"></i></a></li>
+                <li><a href=""><i class="fa fa-instagram"></i></a></li>
+                <li><a href=""><i class="fa fa-youtube-play"></i></a></li>
+            </ul>
+        </div>
+
+        <div class="noi-dung contact">
+            <h2>Thông Tin Liên Hệ</h2>
+            <ul class="info" style="padding-left: 0px;">
+                <li>
+                    <span><i class="fa fa-map-marker"></i></span>
+                    <span>Trường Đại học Bách Khoa, ĐHQG TP. Hồ Chí Minh<br />
+                    268 Lý Thường Kiệt, P.14, Q.10, TP. Hồ Chí Minh<br />
+                    Việt Nam</span>
+                </li>
+                <li>
+                    <span><i class="fa fa-phone"></i></span>
+                        <p><a href="#">0123 456 789</a>
+                        <br />
+                        <a href="#">0123 654 987</a></p>
+                </li>
+                <li>
+                    <span><i class="fa fa-envelope"></i></span>
+                    <p><a href="#">bku@gmail.com</a></p>
+                </li>
+            </ul>
+        </div>
+    </div>
+</footer>
