5bc9948ee9e06ae7be446f28a91707eaeafef714
GrampHoang <92806124+GrampHoang@users.noreply.github.com>
2021-11-22 17:26:43 +0700

    Fix OM php

--- mvc/views/getOrderAjax.php
+++ mvc/views/getOrderAjax.php
@@ -2,7 +2,7 @@
     $output = '';
     foreach($orders as $order){
         $output .= '
-        <div id="item1" class="col">            
+        <div id="item1" class="orderbox">            
             <div class="b4-grid-row">
                 <div class="header_cart">
                     <i class="bi bi-cart-check-fill"></i>
@@ -15,15 +15,15 @@
                         <div class="item_list_detail" id="'.$food['ID'].'">
                             <img src="./assets/img/'.$food['img'].'.png" alt="">
                             <div class="item_list_content">
-                                <div class="item_name">'.$food['Name'].'</div>
-                                <div class="item_quantity">Số lượng: '.$food['quantity'].'</div>
+                                <div class="item_name_OM">'.$food['Name'].'</div>
+                                <div class="item_quantity_OM">Số lượng: '.$food['quantity'].'</div>
                             </div>
                         </div>
                     </div>';
         }   
         $output .= '
                 </div>
-                <br><button data-id='.$order['ID'].' class="button-finish"> Finish</button><br> 
+                <br><button data-id='.$order['ID'].' class="finish_food"> Finish</button><br> 
             </div>
         </div>';
     }
