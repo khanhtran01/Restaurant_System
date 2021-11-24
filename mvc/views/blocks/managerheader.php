e5c50bd07a87a09f7c91665232bd9b7648961981
GrampHoang <92806124+GrampHoang@users.noreply.github.com>
2021-11-24 20:57:06 +0700

    OM fix again

--- mvc/views/blocks/managerheader.php
+++ mvc/views/blocks/managerheader.php
@@ -3,18 +3,6 @@
         <div class="grid__row">
             <div class="nav_bar_up">
                 <div class="nav_bar_up_info">
-                    <div class="nav_bar_up_info_telephone">
-                        <i class="bi bi-phone"></i>
-                        <div>0392747972</div>
-                    </div>
-                    <div class="nav_bar_up_info_email">
-                        <i class="bi bi-envelope"></i>
-                        <div>bku@hcmut.edu.vn</div>
-                    </div>
-                    <div class="nav_bar_up_info_location">
-                        <i class="bi bi-geo-alt"></i>
-                        <div>Địa chỉ</div>
-                    </div>
                 </div>
                 <div class="nav_bar_up_login">
                     <a >Xin chào: <?= $_SESSION['ho'] ?> <?= $_SESSION['ten'] ?></a>
